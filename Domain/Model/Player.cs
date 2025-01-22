24399ed3355309601d56c3f6508da13a6a9aa657
Daniel Kohl <daniel.kohl@tu-dortmund.de>
2025-01-22 21:38:50 +0100

    AddDomainObjects

---
+++ Domain/Model/Player.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Model.Common;
+
+namespace Domain.Model;
+
+public class Player : Entity
+{
+    public required string Name { get; set; }
+    public required Position Position { get; set; }
+    public required Guid TeamId { get; set; }
+}
---
+++ Domain/Model/Team.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Model.Common;
+
+namespace Domain.Model;
+
+public class Team : Entity
+{
+    public required string Name { get; set; }
+    public required Guid LeagueId { get; set; }
+    public required List<Player> Players { get; set; }
+}
