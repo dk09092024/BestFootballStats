c6e9bc52f690897c96ff1621213294507c36d3c1
Daniel Kohl <daniel.kohl@tu-dortmund.de>
2025-02-02 22:36:28 +0100

    Refactor Team and Player models to use nullable LeagueId and TeamId; update configurations and add initial database migration

---
+++ Infrastructure/Database/Migrations/20250202210355_Initial.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "BestFootballStatsApp");
+
+            migrationBuilder.CreateTable(
+                name: "Entity",
+                schema: "BestFootballStatsApp",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "leagues",
+                schema: "BestFootballStatsApp",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    name = table.Column<string>(type: "varchar", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_leagues", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_leagues_Entity_Id",
+                        column: x => x.Id,
+                        principalSchema: "BestFootballStatsApp",
+                        principalTable: "Entity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "teams",
+                schema: "BestFootballStatsApp",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    name = table.Column<string>(type: "varchar", maxLength: 100, nullable: false),
+                    league_id = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_teams", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_teams_Entity_Id",
+                        column: x => x.Id,
+                        principalSchema: "BestFootballStatsApp",
+                        principalTable: "Entity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_teams_leagues_league_id",
+                        column: x => x.league_id,
+                        principalSchema: "BestFootballStatsApp",
+                        principalTable: "leagues",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "matches",
+                schema: "BestFootballStatsApp",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    home_team_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    away_team_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    total_passes = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_matches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_matches_Entity_Id",
+                        column: x => x.Id,
+                        principalSchema: "BestFootballStatsApp",
+                        principalTable: "Entity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_matches_teams_away_team_id",
+                        column: x => x.away_team_id,
+                        principalSchema: "BestFootballStatsApp",
+                        principalTable: "teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_matches_teams_home_team_id",
+                        column: x => x.home_team_id,
+                        principalSchema: "BestFootballStatsApp",
+                        principalTable: "teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "players",
+                schema: "BestFootballStatsApp",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    name = table.Column<string>(type: "varchar", maxLength: 100, nullable: false),
+                    position = table.Column<string>(type: "varchar", nullable: false),
+                    team_id = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_players", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_players_Entity_Id",
+                        column: x => x.Id,
+                        principalSchema: "BestFootballStatsApp",
+                        principalTable: "Entity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_players_teams_team_id",
+                        column: x => x.team_id,
+                        principalSchema: "BestFootballStatsApp",
+                        principalTable: "teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_matches_away_team_id",
+                schema: "BestFootballStatsApp",
+                table: "matches",
+                column: "away_team_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_matches_home_team_id",
+                schema: "BestFootballStatsApp",
+                table: "matches",
+                column: "home_team_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_players_team_id",
+                schema: "BestFootballStatsApp",
+                table: "players",
+                column: "team_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_teams_league_id",
+                schema: "BestFootballStatsApp",
+                table: "teams",
+                column: "league_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "matches",
+                schema: "BestFootballStatsApp");
+
+            migrationBuilder.DropTable(
+                name: "players",
+                schema: "BestFootballStatsApp");
+
+            migrationBuilder.DropTable(
+                name: "teams",
+                schema: "BestFootballStatsApp");
+
+            migrationBuilder.DropTable(
+                name: "leagues",
+                schema: "BestFootballStatsApp");
+
+            migrationBuilder.DropTable(
+                name: "Entity",
+                schema: "BestFootballStatsApp");
+        }
+    }
+}
